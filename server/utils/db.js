ca599ae0446c27ea90ded0ec9e10d4a3efda9399
Sasha Chekh <oleksandr.chekh@chisw.com>
2023-06-15 21:49:46 +0300

    fix: update volumes for docker-compose.yml

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
-// const path = require('path');
-// require('dotenv').config({path: path.join(__dirname, '../.env')});
+const path = require('path');
+require('dotenv').config({path: '../.env'});
 
 const {DATABASE_USER, DATABASE_DBNAME, DATABASE_PASSWORD} = process.env;
-console.log('DATABASE: ', process.env)
+
 
 // const sequelize =
 //     new Sequelize(DATABASE_DBNAME, DATABASE_USER, DATABASE_PASSWORD,
