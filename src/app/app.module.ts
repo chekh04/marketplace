fe2920e3b835b70a68b3c8423c2a79e873600184
Sasha Chekh <oleksandr.chekh@chisw.com>
2023-06-24 16:56:47 +0300

    feat: move applicaton to pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import {HttpClientModule} from "@angular/common/http";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,14 @@ import {HttpClientModule} from "@angular/common/http";
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    AngularSvgIconModule.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -3,13 +3,15 @@ import {ClientComponent} from "./client.component";
 import {CommonModule} from "@angular/common";
 import {ClientRoutingModule} from "./client-routing.module";
 import {HeaderModule} from "./components/header/header.module";
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @NgModule({
   declarations: [ClientComponent],
   imports: [
     CommonModule,
     ClientRoutingModule,
-    HeaderModule
+    HeaderModule,
+    AngularSvgIconModule
   ]
 })
 
